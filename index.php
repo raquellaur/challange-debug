8b7d309c19ecb783b7c8ccf56d6ce043fa81d197
raquellaur <70804673+raquellaur@users.noreply.github.com>
2020-10-25 22:00:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$characters = [
+    "Raquel Laur"  => [
+        "city" => "Lyon",
+        "weapon" => "flamethrower"
+    ],
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ]
+];
+
+var_dump($characters);
